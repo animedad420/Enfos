43c122fedc88ce2ed827890de7121ce96ce9d83f
Justyn <thisfieldhasbeenleft@intentionally.blank>
2019-09-22 17:59:23 +1000

    Soul Feast resets the stolen ability's cooldown before the ability is used, doing it after doesn't work

--- game/dota_addons/enfos/scripts/vscripts/abilities/troll_cannibal/modifier_soul_feast_lua.lua
+++ game/dota_addons/enfos/scripts/vscripts/abilities/troll_cannibal/modifier_soul_feast_lua.lua
@@ -74,17 +74,20 @@ function modifier_soul_feast_lua:TrollUltSteal(victim)
 	if name == "npc_dota_hero_juggernaut" or name == "npc_dota_hero_terrorblade" 
 	or name == "npc_dota_hero_beastmaster" or name == "npc_dota_hero_spirit_breaker" then spell = target:GetAbilityByIndex(2) end
 	if name == "npc_dota_hero_silencer" then spell = target:GetAbilityByIndex(0) end
-	local spellLevel = caster:FindAbilityByName("troll_cannibal_soul_feast"):GetLevel()
-	caster:RemoveAbility("troll_cannibal_soul_feast")
-	caster:AddAbility(spell:GetAbilityName())
-	caster:FindAbilityByName(spell:GetAbilityName()):SetLevel(spellLevel)
-	if name == "npc_dota_hero_luna" or name == "npc_dota_hero_silencer" then
-		caster:RemoveAbility("troll_cannibal_soul_drain")
-		caster:AddAbility("generic_focus_moonbeam")
-		caster:FindAbilityByName("generic_focus_moonbeam"):SetLevel(1)
-	end
-	if name == "npc_dota_hero_sniper" or name == "npc_dota_hero_antimage" then
-		caster:AddNewModifier(caster,self:GetAbility(),"modifier_soul_feast_passive_timer",{duration = 20})
+	if caster:FindAbilityByName("troll_cannibal_soul_feast") ~= nil then
+		local spellLevel = caster:FindAbilityByName("troll_cannibal_soul_feast"):GetLevel()
+		caster:RemoveAbility("troll_cannibal_soul_feast")
+		caster:AddAbility(spell:GetAbilityName())
+		caster:FindAbilityByName(spell:GetAbilityName()):SetLevel(spellLevel)
+		caster:FindAbilityByName(spell:GetAbilityName()):EndCooldown()
+		if name == "npc_dota_hero_luna" or name == "npc_dota_hero_silencer" then
+			caster:RemoveAbility("troll_cannibal_soul_drain")
+			caster:AddAbility("generic_focus_moonbeam")
+			caster:FindAbilityByName("generic_focus_moonbeam"):SetLevel(1)
+		end
+		if name == "npc_dota_hero_sniper" or name == "npc_dota_hero_antimage" then
+			caster:AddNewModifier(caster,self:GetAbility(),"modifier_soul_feast_passive_timer",{duration = 20})
+		end
 	end
 end
 
@@ -97,10 +100,12 @@ function modifier_soul_feast_lua:ResetTrollUlt(forcereset)
 	if spell:GetChannelTime() > 0 then
 		if not forcereset then return nil end
 	end
+	--caster:FindAbilityByName(spell:GetAbilityName()):EndCooldown()
 	caster:RemoveAbility(spell:GetAbilityName())
 	caster:AddAbility("troll_cannibal_soul_feast")
 	caster:FindAbilityByName("troll_cannibal_soul_feast"):SetLevel(spellLevel)
 	if caster:FindAbilityByName("generic_focus_moonbeam") ~= nil then
+		caster:FindAbilityByName("generic_focus_moonbeam"):EndCooldown()
 		caster:RemoveAbility("generic_focus_moonbeam")
 		caster:AddAbility("troll_cannibal_soul_drain")
 		caster:FindAbilityByName("troll_cannibal_soul_drain"):SetLevel(1)
